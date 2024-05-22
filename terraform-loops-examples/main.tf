c7012a0a98c48b5f35ecc2e8a1806d6fe8a626b9
Prateekkumawat <prateekkumawat20@gmail.com>
2024-05-22 19:26:03 +0530

    loops with count examples
    loops examples count functions and legnth

---
+++ terraform-loops-examples/main.tf
@@ -0,0 +1,155 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.48.0"
+    }
+  }
+}
+
+# create aws provider 
+provider "aws" {
+  region = "ap-south-1"
+  secret_key = "xxxx"
+  access_key = "xxxx"
+}
+
+# create a vpc for aws project 
+resource "aws_vpc" "hsitmanagementvpc" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  lifecycle {
+    prevent_destroy =  false
+    }
+  tags = {
+    Name = "hsit-manangement-vpc"
+  }
+}
+
+# crate a subnet1 for public
+resource "aws_subnet" "hsitmanagementsubnet1" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.hsitmanagementvpc.id
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "hsit-management-public1-az1"
+  }
+}
+
+# create a internet gateway for vpc 
+resource "aws_internet_gateway" "hsitmanagementigw" {
+  vpc_id = aws_vpc.hsitmanagementvpc.id
+
+  tags = {
+    Name = "hsit-management-vpc-igw"
+  }
+}
+
+# create a public route table for managmement vpc
+resource "aws_route_table" "hsitmanagenmentpublicrt" {
+  vpc_id = aws_vpc.hsitmanagementvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.hsitmanagementigw.id
+  }
+  tags = {
+    Name = "hsit-managment-public-rt"
+  }
+}
+
+# crate a subnet assosiation for public rt
+resource "aws_route_table_association" "publicrtsubnet1assosiation" {
+  subnet_id      = aws_subnet.hsitmanagementsubnet1.id
+  route_table_id = aws_route_table.hsitmanagenmentpublicrt.id
+}
+
+# Genrate a key for Instance  
+resource "tls_private_key" "hsitpublic_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096 
+}
+resource "aws_key_pair" "hsitpublic_key_pair" {
+  key_name   = "hsitpublicins.pem"
+  public_key = tls_private_key.hsitpublic_key.public_key_openssh
+}
+resource "local_file" "hsitpublicserverkey" {
+   filename = "hsitpublicins.pem"
+   content = tls_private_key.hsitpublic_key.private_key_pem
+}
+
+
+# crate a public server in vpc
+resource "aws_instance" "hsitmanagementpublicserver" {
+  count =  length(var.testbox)
+  ami = "ami-013e83f579886baeb"
+  instance_type = "t2.small"
+  key_name = aws_key_pair.hsitpublic_key_pair.key_name
+  security_groups = [aws_security_group.hsitmanagmentpublicsg.id]
+  subnet_id = aws_subnet.hsitmanagementsubnet1.id
+  lifecycle {
+    create_before_destroy  = true
+    #prevent_destroy = true
+    ignore_changes = all
+  }
+  tags = {
+    Name = var.testbox[count.index]
+  }
+}
+
+
+# create security group for public servers
+resource "aws_security_group" "hsitmanagmentpublicsg" {
+  name        = "hsit-management-public-server-sg"
+  description = "Security Group for public servers hsit-management-vpc "
+  vpc_id = aws_vpc.hsitmanagementvpc.id
+
+    ingress {
+      description = "Allow port ssh"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      description = "Allow port http"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  egress {
+    description = "Allow ALL ports"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "publicservereip0" {
+   value = aws_instance.hsitmanagementpublicserver[0].public_ip
+}
+
+output "privateserverip0" {
+  value = aws_instance.hsitmanagementpublicserver[0].private_ip
+}
+
+output "publicservereip1" {
+   value = aws_instance.hsitmanagementpublicserver[1].public_ip
+}
+
+output "privateserverip1" {
+  value = aws_instance.hsitmanagementpublicserver[1].private_ip
+}
+
+output "publicservereip2" {
+   value = aws_instance.hsitmanagementpublicserver[2].public_ip
+}
+
+output "privateserverip2" {
+  value = aws_instance.hsitmanagementpublicserver[2].private_ip
+}
---
+++ terraform-loops-examples/vars.tf
@@ -0,0 +1,4 @@
+variable "testbox" {
+  type = list(string)
+  default = [ "testbox1", "testbox2", "testbox3", "testbox4", "testbox5" ]
+}
